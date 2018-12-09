4e66fb760bcd6862784660721111cc91baac5ad0
ClementBrd <37980556+ClementBrd@users.noreply.github.com>
2018-12-09 16:48:14 +0100

    Add files via upload

---
+++ bddcdc.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 09 déc. 2018 à 15:45
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bddcdc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jeu`
+--
+
+DROP TABLE IF EXISTS `jeu`;
+CREATE TABLE IF NOT EXISTS `jeu` (
+  `idJoueur` int(11) NOT NULL,
+  `nom` char(100) NOT NULL,
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idJoueur`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
